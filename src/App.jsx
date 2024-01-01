63485473e4fd8072a92d23edbea0e70163dff97e
Steve Lombardi <smlombardi@gmail.com>
2024-01-01 10:44:09 -0500

    Refactor CreateOrder component and add action for form submission

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import Order, { loader as orderLoader } from "./features/order/Order";
-import CreateOrder from "./features/order/CreateOrder";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error";
 
@@ -20,7 +22,12 @@ const router = createBrowserRouter([
         errorElement: <Error />,
       },
       { path: "/cart", element: <Cart /> },
-      { path: "/order/new", element: <CreateOrder /> },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+        action: createOrderAction,
+        errorElement: <Error />,
+      },
       {
         path: "/order/:orderId",
         element: <Order />,
