dd01afc3eb2f2bf90882c28ecf566b5634366e94
Steve Lombardi <smlombardi@gmail.com>
2024-01-01 14:50:51 -0500

    Update page titles and styles, add user creation form

---
+++ src/features/user/UserName.jsx
@@ -0,0 +1,3 @@
+export default function UserName() {
+  return <div className="text-sm font-semibold">Melanie Daniels</div>;
+}
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import UserName from "../features/user/UserName";
 
 export default function Header() {
   return (
-    <header>
-      <Link to="/">Fast React Pizza Co.</Link>
+    <header className="flex items-center justify-between px-4 py-3 uppercase bg-yellow-500 border-b border-stone-200 sm:px-6">
+      <Link to="/" className="tracking-widest">
+        {" "}
+        Fast React Pizza Co.
+      </Link>
       <SearchOrder />
+      <UserName />
     </header>
   );
 }
