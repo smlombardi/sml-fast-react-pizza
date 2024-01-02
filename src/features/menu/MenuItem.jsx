0129a8d365a83528b93e9cd8290dca65a70bf1df
Steve Lombardi <smlombardi@gmail.com>
2024-01-02 17:01:47 -0500

    Refactor menu and menu item components

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -4,13 +4,25 @@ function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
   return (
-    <li>
-      <img src={imageUrl} alt={name} />
-      <div>
-        <p>{name}</p>
-        <p>{ingredients.join(", ")}</p>
-        <div>
-          {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
+    <li className="flex gap-4 py-2">
+      <img
+        src={imageUrl}
+        alt={name}
+        className={`h-24 ${soldOut ? "opacity-60 grayscale" : ""}`}
+      />
+      <div className="flex flex-col">
+        <p className="font-bold">{name}</p>
+        <p className="text-sm capitalize text-stone-400">
+          {ingredients.join(", ")}
+        </p>
+        <div className="mt-auto text-sm">
+          {!soldOut ? (
+            <p className="text-sm">{formatCurrency(unitPrice)}</p>
+          ) : (
+            <p className="text-sm font-bold uppercase text-stone-400">
+              Sold out
+            </p>
+          )}
         </div>
       </div>
     </li>
