160e8e57217d7a1d61d3cb52e863556b893b8d73
Steve Lombardi <smlombardi@gmail.com>
2024-01-02 09:21:36 -0500

    Update button styles in CreateOrder component

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -79,7 +79,7 @@ function CreateOrder() {
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
           <button
             disabled={isSubmitting}
-            className="inline-block px-4 py-2 mt-8 font-semibold tracking-wide bg-yellow-400 rounded-full text-stone-800 hover:bg-yellow-500 focus:outline-none focus:ring"
+            className="inline-block px-4 py-2 mt-8 font-semibold tracking-wide transition-colors duration-300 bg-yellow-400 rounded-full text-stone-800 hover:bg-yellow-500 focus:outline-none focus:ring focus:ring-offset-2 focus:ring-yellow-400 focus:bg-yellow-300 disabled:opacity-50 disabled:cursor-not-allowed"
           >
             {isSubmitting ? "Placing Order..." : "Order now"}
           </button>
