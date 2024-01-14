32517342b77d0d1510abdbf74096b7c7235b43bd
Steve Lombardi <smlombardi@gmail.com>
2024-01-14 11:56:05 -0500

    Add cartReducer to store configuration

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cart: [],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem: (state, action) => {
+      state.cart.push(action.payload);
+    },
+    deleteItem: (state, action) => {
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+    increaseItemQuantity: (state, action) => {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity++;
+      item.totalPrice = item.quantity * item.price;
+    },
+    decreaseItemQuantity: (state, action) => {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity--;
+    },
+    clearCart: (state) => {
+      state.cart = [];
+    },
+  },
+});
+
+export const {
+  addItem,
+  deleteItem,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+  clearCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
