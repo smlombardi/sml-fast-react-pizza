7b6b0f707f88c1c5bcab0b596c6fe134c2203261
Steve Lombardi <smlombardi@gmail.com>
2024-01-15 13:17:46 -0500

    Refactor CreateOrder component and fix OrderItem key

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
+import EmptyCart from "../cart/EmptyCart";
 import { useSelector } from "react-redux";
+import { getCart } from "../cart/cartSlice";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -10,37 +11,15 @@ const isValidPhone = (str) =>
     str
   );
 
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: "Mediterranean",
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: "Vegetale",
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: "Spinach and Mushroom",
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
-
 function CreateOrder() {
   // const [withPriority, setWithPriority] = useState(false);
-  const cart = fakeCart;
+  const cart = useSelector(getCart);
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
   const formErrors = useActionData();
   const username = useSelector((state) => state.user.username);
+
+  if (!cart.length) return <EmptyCart />;
   return (
     <div>
       <h1 className="mb-8 text-xl font-semibold">Ready to order? Lets go!</h1>
@@ -108,7 +87,7 @@ export async function action({ request }) {
   };
 
   const errors = {};
-  if (!isValidPhone(order.customer.phone)) {
+  if (!isValidPhone(order.phone)) {
     errors.phone = "Invalid phone number";
   }
   if (Object.keys(errors).length > 0) {
--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -51,7 +51,7 @@ function Order() {
 
       <ul className="border-t border-b divide-y divide-stone-200">
         {cart.map((item) => (
-          <OrderItem item={item} key={item.id} />
+          <OrderItem item={item} key={item.pizzaId} />
         ))}
       </ul>
 
