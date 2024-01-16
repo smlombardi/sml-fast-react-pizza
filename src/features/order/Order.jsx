2a324bda3350cc6e945b4dead784529c4df9f2e1
Steve Lombardi <smlombardi@gmail.com>
2024-01-16 11:01:42 -0500

    Add update order action and component

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -9,6 +9,7 @@ import {
   formatDate,
 } from "../../utils/helpers";
 import { useEffect } from "react";
+import UpdateOrder from "./UpdateOrder";
 
 function Order() {
   const order = useLoaderData();
@@ -83,6 +84,7 @@ function Order() {
           To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
         </p>
       </div>
+      {!priority && <UpdateOrder order={order} />}
     </div>
   );
 }
